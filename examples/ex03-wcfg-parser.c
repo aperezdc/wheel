561680547f8f0723892b62c453619d331471ff6d
Adrian Perez <aperez@igalia.com>
2010-09-09 04:48:23 +0200

    Config parser and config file input examples
    The ex03-wcfg-parser.c program will just parse stdin as a configuration
    file, printing errors if there are some. Return code is zero for successful
    parse and non-zero for errors.

---
+++ examples/ex03-wcfg-parser.c
@@ -0,0 +1,38 @@
+/*
+ * ex03-wcfg-parser.c
+ * Copyright (C) 2010 Adrian Perez <aperez@igalia.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "wmem.h"
+#include "wcfg.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int
+main (int argc, char **argv)
+{
+    w_cfg_parser_t parser;
+    char *msg;
+
+    (void) argc;
+    (void) argv;
+
+    w_cfg_parser_init (&parser, stdin);
+    msg = w_cfg_parser_parse (&parser);
+
+    if (msg != NULL) {
+        fprintf (stderr, "<stdin>:%s\n", msg);
+        w_free (msg);
+        exit (EXIT_FAILURE);
+    }
+    else {
+        w_cfg_free (parser.result);
+    }
+
+    exit (EXIT_SUCCESS);
+    return EXIT_SUCCESS;
+}
+
