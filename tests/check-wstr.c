baf5866d1de848b41b8e9f2b2fa39be994ac0132
Adrian Perez <aperez@igalia.com>
2010-10-08 21:02:50 +0200

    tests: Simple test cases for w_strdup(), w_strndup()

--- tests/check-wstr.c
+++ tests/check-wstr.c
@@ -9,13 +9,36 @@
 #include "wheel.h"
 
 
-START_TEST (test_wstr_hash_neq) {
+START_TEST (test_wstr_hash_neq)
+{
     fail_if (w_hashstr ("foo") == w_hashstr ("bar"),
              "same hash produced for different strings");
-} END_TEST
+}
+END_TEST
 
-START_TEST (test_wstr_hash_eq) {
+
+START_TEST (test_wstr_hash_eq)
+{
     fail_if (w_hashstr ("foo") != w_hashstrn ("foo", 3),
              "w_hashstrn and w_hashstr produced different hashes");
-} END_TEST
+    fail_if (w_hashstr ("foo") != w_hashstrn ("foobar", 3),
+             "w_hashstrn and w_hashstr produced different hashes");
+}
+END_TEST
+
+
+START_TEST (test_wstr_dup)
+{
+    fail_if (strcmp ("foo", w_strdup ("foo")),
+             "w_strndup: cloned string does not match");
+}
+END_TEST
+
+
+START_TEST (test_wstr_ndup)
+{
+    fail_if (strcmp ("foo", w_strndup ("foobar", 3)),
+             "w_strndup: cloned string does not match");
+}
+END_TEST
 
