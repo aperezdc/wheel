9080a49f7fbe46ac5b0a039a8a3872a94e614f30
Adrian Perez <aperez@igalia.com>
2010-10-18 23:52:35 +0300

    Implement wcfg example which dumps input to stdout
    This may be used to test out wcfg config file format parsing.

---
+++ examples/wcfg-noop.c
@@ -0,0 +1,32 @@
+/*
+ * wcfg-noop.c
+ * Copyright (C) 2010 Adrian Perez <aperez@igalia.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "wheel.h"
+
+
+int
+main (int argc, char **argv)
+{
+    char    *err = NULL;
+    w_cfg_t *cfg;
+
+    if (argc != 2)
+        w_die ("usage: %s <conf-file>\n", argv[0]);
+
+    if ((cfg = w_cfg_load_file (argv[1], &err)) == NULL) {
+        w_assert (err);
+        w_die ("%s:%s\n", argv[1], err);
+    }
+
+    w_free (err);
+
+    w_cfg_dump (cfg, stdout);
+
+    return EXIT_SUCCESS;
+}
+
+
