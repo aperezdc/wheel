016eef5c24bf46799f4781ae3445da7c8050d140
Adrian Perez <aperez@igalia.com>
2010-11-04 23:20:17 +0200

    Test case for W_OPT_INT

--- tests/check-wopt.c
+++ tests/check-wopt.c
@@ -28,3 +28,26 @@ START_TEST (test_wopt_set_bool)
 END_TEST
 
 
+START_TEST (test_wopt_set_int)
+{
+    int value;
+    const w_opt_t options[] = {
+        { 1, 'i', "int", W_OPT_INT, &value, "Int value" },
+        W_OPT_END
+    };
+    static char *argv1[] = { "prog", "-i", "42" };
+    static char *argv2[] = { "prog", "-i", "-42" };
+    static char *argv3[] = { "prog", "-i", "0x42" };
+
+    w_opt_parse (options, NULL, NULL, w_lengthof (argv1), argv1);
+    fail_unless (value == 42, "expected 42, got %i", value);
+
+    w_opt_parse (options, NULL, NULL, w_lengthof (argv2), argv2);
+    fail_unless (value == -42, "expected -42, got %i", value);
+
+    w_opt_parse (options, NULL, NULL, w_lengthof (argv3), argv3);
+    fail_unless (value == 0x42, "expected 0x42, got %#x", value);
+}
+END_TEST
+
+
