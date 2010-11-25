cc17a14cb5035973f83a10af82783a8e14470839
Adrian Perez <aperez@igalia.com>
2010-11-25 10:35:58 +0100

    Simple wobj example

---
+++ examples/wobj-simple.c
@@ -0,0 +1,41 @@
+/*
+ * wobj-simple.c
+ * Copyright (C) 2010 Adrian Perez <aperez@igalia.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "wheel.h"
+
+W_OBJ (my_obj_t)
+{
+    w_obj_t parent;
+    int     val;
+};
+
+
+static my_obj_t*
+my_obj_new (int val)
+{
+    my_obj_t *obj = w_obj_new (my_obj_t);
+    obj->val = val;
+    return obj;
+}
+
+
+int
+main (int argc, char **argv)
+{
+    my_obj_t *obj;
+
+    w_unused (argc);
+    w_unused (argv);
+
+    obj = my_obj_new (10);
+    printf ("%i\n", obj->val);
+    w_obj_unref (obj);
+
+    return EXIT_SUCCESS;
+}
+
+
