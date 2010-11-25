f9d4d27560be4f47da9d1c6d07b3eb92cabdd668
Adrian Perez <aperez@igalia.com>
2010-11-25 16:14:59 +0100

    Update unit tests for w_io_buf_t

--- tests/check-wiobuf.c
+++ tests/check-wiobuf.c
@@ -19,26 +19,25 @@ static const char *msg = "Too much work and no joy makes Jack a dull boy.\n"
 
 START_TEST (test_wio_buf_open_empty)
 {
-    W_IO_BUF (io);
-    w_io_buf_open (io, NULL);
+    w_io_t *io = w_io_buf_open (NULL);
 
     fail_unless (W_IO_BUF_BUF (io) != NULL,
                  "I/O has no autocreated buffer");
     fail_unless (w_buf_length (W_IO_BUF_BUF (io)) == 0,
                  "I/O autocreated buffer does not have zero length");
 
-    w_io_close (io);
+    w_obj_unref (io);
 }
 END_TEST
 
 
 START_TEST (test_wio_buf_open_nonempty)
 {
-    W_IO_BUF (io);
+    w_io_t *io;
     w_buf_t b = W_BUF;
 
     w_buf_set_str (&b, msg);
-    w_io_buf_open (io, &b);
+    io = w_io_buf_open (&b);
 
     fail_unless (W_IO_BUF_BUF (io) != NULL,
                 "I/O has no buffer");
@@ -48,7 +47,7 @@ START_TEST (test_wio_buf_open_nonempty)
                  w_buf_length (W_IO_BUF_BUF (io)),
                  strlen (msg));
 
-    w_io_close (io);
+    w_obj_unref (io);
 }
 END_TEST
 
@@ -57,11 +56,11 @@ START_TEST (test_wio_buf_read)
 {
     char buf[64];
     ssize_t ret;
-    W_IO_BUF (io);
+    w_io_t *io;
     w_buf_t b = W_BUF;
 
     w_buf_set_str (&b, msg);
-    w_io_buf_open (io, &b);
+    io = w_io_buf_open (&b);
 
     ret = w_io_read (io, buf, 64);
     fail_unless (ret == 64, "read %ld bytes, expected 64", (long) ret);
@@ -71,17 +70,17 @@ START_TEST (test_wio_buf_read)
     fail_unless (ret == 10, "read %ld bytes, expected 10", (long) ret);
     fail_if (memcmp (msg + 64, buf, 10), "strings are not equal");
 
-    w_io_close (io);
+    w_obj_unref (io);
 }
 END_TEST
 
 
 START_TEST (test_wio_buf_write)
 {
-    W_IO_BUF (io);
+    w_io_t *io;
     w_buf_t *b;
 
-    w_io_buf_open (io, NULL);
+    io = w_io_buf_open (NULL);
     w_io_write (io, msg, 10);
     w_io_write (io, msg, 10);
     w_io_write (io, msg, 10);
@@ -97,7 +96,7 @@ START_TEST (test_wio_buf_write)
     fail_if (memcmp (b->buf + 10, msg, 10), "buffers do not match");
     fail_if (memcmp (b->buf + 20, msg, 10), "buffers do not match");
 
-    w_io_close (io);
+    w_obj_unref (io);
 }
 END_TEST
 
