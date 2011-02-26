4834014fdcb5d993c3d01e04b30d72f082d211dd
Adrian Perez <aperez@igalia.com>
2011-02-26 03:38:15 +0200

    wopt: Improve code styling, mostly indentation

--- wopt.c
+++ wopt.c
@@ -22,18 +22,18 @@
  *   - func   Name of the primitive function used to convert a single item.
  *            For examples take a look at "wheel/wstr.h".
  */
-#define _MAKE_CONVERTER(name, type, func)                                    \
-	w_opt_status_t name (const w_opt_context_t *context) {                     \
-		unsigned i;                                                              \
-		w_assert(context != NULL);                                               \
-		w_assert(context->option != NULL);                                       \
-		w_assert(context->option->narg > 0);                                     \
-		w_assert(context->option->extra != NULL);                                \
-		for (i = 0; i < context->option->narg; i++)                              \
-			if (!func(context->argument[i], &((type*) context->option->extra)[i])) \
-				return W_OPT_BAD_ARG;                                                \
-		return W_OPT_OK;                                                         \
-	}
+#define _MAKE_CONVERTER(name, type, func)                                       \
+    w_opt_status_t name (const w_opt_context_t *context) {                      \
+        unsigned i;                                                             \
+        w_assert (context != NULL);                                             \
+        w_assert (context->option != NULL);                                     \
+        w_assert (context->option->narg > 0);                                   \
+        w_assert (context->option->extra != NULL);                              \
+        for (i = 0; i < context->option->narg; i++)                             \
+        if (!func (context->argument[i], &((type*) context->option->extra)[i])) \
+            return W_OPT_BAD_ARG;                                               \
+        return W_OPT_OK;                                                        \
+    }
 
 
 _MAKE_CONVERTER( W_OPT_INT   , int          , w_str_int    )
@@ -52,10 +52,10 @@ _MAKE_CONVERTER( W_OPT_DOUBLE, double       , w_str_double )
 
 
 w_opt_status_t
-w_opt_files_action(const w_opt_context_t *ctx)
+w_opt_files_action (const w_opt_context_t *ctx)
 {
-	w_unused(ctx);
-	return W_OPT_FILES;
+    w_unused (ctx);
+    return W_OPT_FILES;
 }
 
 
@@ -65,23 +65,23 @@ w_opt_files_action(const w_opt_context_t *ctx)
  * boolean value will be toggled when this function is applied).
  */
 w_opt_status_t
-W_OPT_BOOL(const w_opt_context_t *context)
+W_OPT_BOOL (const w_opt_context_t *context)
 {
-	unsigned i;
-	w_assert(context != NULL);
-	w_assert(context->option != NULL);
-	w_assert(context->option->extra != NULL);
+    unsigned i;
+    w_assert (context != NULL);
+    w_assert (context->option != NULL);
+    w_assert (context->option->extra != NULL);
 
-	if (context->option->narg == 0) {
-		*((wbool*) context->option->extra) = W_YES;
-		return W_OPT_OK;
+    if (context->option->narg == 0) {
+        *((wbool*) context->option->extra) = W_YES;
+        return W_OPT_OK;
 	}
 
-	for (i = 0; i < context->option->narg; i++)
-		if (!w_str_bool(context->argument[i], &((wbool*) context->option->extra)[i]))
-			return W_OPT_BAD_ARG;
+    for (i = 0; i < context->option->narg; i++)
+        if (!w_str_bool (context->argument[i], &((wbool*) context->option->extra)[i]))
+            return W_OPT_BAD_ARG;
 
-	return W_OPT_OK;
+    return W_OPT_OK;
 }
 
 
@@ -91,193 +91,193 @@ W_OPT_BOOL(const w_opt_context_t *context)
  * taking care of freeing that.
  */
 w_opt_status_t
-W_OPT_STRING(const w_opt_context_t *context)
+W_OPT_STRING (const w_opt_context_t *context)
 {
-	unsigned i;
-	w_assert(context != NULL);
-	w_assert(context->option != NULL);
-	w_assert(context->option->narg > 0);
-	w_assert(context->option->extra != NULL);
+    unsigned i;
+    w_assert (context != NULL);
+    w_assert (context->option != NULL);
+    w_assert (context->option->narg > 0);
+    w_assert (context->option->extra != NULL);
 
-	for (i = 0; i < context->option->narg; i++)
-		((char**) context->option->extra)[i] = w_strdup(context->argument[i]);
+    for (i = 0; i < context->option->narg; i++)
+        ((char**) context->option->extra)[i] = w_strdup (context->argument[i]);
 
-	return W_OPT_OK;
+    return W_OPT_OK;
 }
 
 
 static inline const char*
 _program_name (const char *str)
 {
-  /* FIXME Using a slash literal Unixish! */
-  const char *slash = strrchr (str, '/');
-  return (slash == NULL) ? str : (slash + 1);
+    /* FIXME Using a slash literal Unixish! */
+    const char *slash = strrchr (str, '/');
+    return (slash == NULL) ? str : (slash + 1);
 }
 
 
 static inline size_t
-_longest_option_width(const w_opt_t *opt)
+_longest_option_width (const w_opt_t *opt)
 {
-  size_t longest = 4; /* "help" has 4 characters */
-  size_t cur_len;
-  w_assert(opt != NULL);
+    size_t longest = 4; /* "help" has 4 characters */
+    size_t cur_len;
+    w_assert (opt != NULL);
 
-  for (; opt->string != NULL; opt++) {
-    cur_len = strlen(opt->string);
-    if (cur_len > longest) longest = cur_len;
-  }
+    for (; opt->string != NULL; opt++) {
+        cur_len = strlen (opt->string);
+        if (cur_len > longest) longest = cur_len;
+    }
 
-  return longest;
+    return longest;
 }
 
 
 static inline void
-_print_some_chars(FILE *f, const char *s, size_t n)
+_print_some_chars (FILE *f, const char *s, size_t n)
 {
-  while (n-- && (*s != '\0')) fputc(*s++, f);
+    while (n-- && (*s != '\0')) fputc (*s++, f);
 }
 
 
 static inline void
-_print_blanks(FILE *f, size_t n)
+_print_blanks (FILE *f, size_t n)
 {
-  while (n--) fputc(' ', f);
+    while (n--) fputc (' ', f);
 }
 
 
 static inline void
-_print_lspaced(FILE *f, const char *s, int l)
+_print_lspaced (FILE *f, const char *s, int l)
 {
-  unsigned tty_cols = w_tty_cols() - l;
-  const char *spc = s;
-  unsigned col = 0;
-  int lstart = 1;
-  size_t len;
-  if (l > 65) l = 20;
+    unsigned tty_cols = w_tty_cols () - l;
+    const char *spc = s;
+    unsigned col = 0;
+    int lstart = 1;
+    size_t len;
+    if (l > 65) l = 20;
 
-  /* Reflow words by inserting line breaks at spaces. */
-  while (s != NULL) {
-    spc = strchr(s, (spc == NULL) ? '\0' : ' ');
-    len = (spc == NULL) ? strlen(s) : (size_t)(spc - s);
-    if (lstart) {
-      /* We're at line start, print always. */
-      col = l + len;
-      _print_some_chars(f, s, len);
-      lstart = 0;
+    /* Reflow words by inserting line breaks at spaces. */
+    while (s != NULL) {
+        spc = strchr (s, (spc == NULL) ? '\0' : ' ');
+        len = (spc == NULL) ? strlen (s) : (size_t) (spc - s);
+        if (lstart) {
+            /* We're at line start, print always. */
+            col = l + len;
+            _print_some_chars (f, s, len);
+            lstart = 0;
+        }
+        else if ((col + len) > tty_cols) {
+            /* Advance to next line. */
+            col = l + len;
+            fputc ('\n', f);
+            _print_blanks (f, l);
+            _print_some_chars (f, s, len);
+        }
+        else {
+            col += len;
+            _print_some_chars (f, s, len);
+        }
+        s = (spc == NULL) ? NULL : spc + 1;
+        fputc (' ', f);
     }
-    else if ((col + len) > tty_cols) {
-      /* Advance to next line. */
-      col = l + len;
-      fputc('\n', f);
-      _print_blanks(f, l);
-      _print_some_chars(f, s, len);
-    }
-    else {
-      col += len;
-      _print_some_chars(f, s, len);
-    }
-    s = (spc == NULL) ? NULL : spc + 1;
-    fputc(' ', f);
-  }
-  fputc('\n', f);
+    fputc('\n', f);
 }
-
 
 
 void
-w_opt_help(const w_opt_t *opt, FILE *out, const char *progname)
+w_opt_help (const w_opt_t *opt, FILE *out, const char *progname)
 {
-	int width;
-	w_assert(opt != NULL);
-	w_assert(out != NULL);
+    int width;
+    w_assert (opt != NULL);
+    w_assert (out != NULL);
 
-	width = (int) _longest_option_width(opt);
-	fprintf(out, "Usage: %s [options] [...]\n", progname);
-	fprintf(out, "Command line options:\n\n");
+    width = (int) _longest_option_width (opt);
+    fprintf (out, "Usage: %s [options] [...]\n", progname);
+    fprintf (out, "Command line options:\n\n");
 
-	for (; opt->string != NULL; opt++) {
-
-		if (OPT_LETTER(opt->letter) && opt->string) {
-		    fprintf (out, "  -%c, --%-*s ",
-		             OPT_LETTER(opt->letter),
-		             width, opt->string);
+    for (; opt->string != NULL; opt++) {
+        if (OPT_LETTER (opt->letter) && opt->string) {
+            fprintf (out, "  -%c, --%-*s ",
+                     OPT_LETTER (opt->letter),
+                     width, opt->string);
         }
-		else if (opt->string) {
-			fprintf (out, "       --%-*s ", width, opt->string);
+        else if (opt->string) {
+            fprintf (out, "       --%-*s ", width, opt->string);
         }
-		else {
-			fprintf (out, "  -%c         ", opt->letter);
+        else {
+            fprintf (out, "  -%c         ", opt->letter);
         }
 
-		switch (opt->narg) {
-			case  0: fprintf(out, "      "); break;
-			case  1: fprintf(out, "ARG   "); break;
-			default: fprintf(out, "ARG...");
-		}
-		_print_lspaced(out, opt->info, 15 + width);
-	}
-	fputc('\n', out);
+        switch (opt->narg) {
+            case  0: fprintf (out, "      "); break;
+            case  1: fprintf (out, "ARG   "); break;
+            default: fprintf (out, "ARG...");
+        }
+        _print_lspaced (out, opt->info, 15 + width);
+    }
+    fputc ('\n', out);
 }
 
 
 static inline const w_opt_t*
-_opt_lookup_shrt(const w_opt_t *opt, char chr)
+_opt_lookup_shrt (const w_opt_t *opt, char chr)
 {
-  w_assert(opt != NULL);
-  for (; opt->string != NULL; opt++)
-    if (chr == OPT_LETTER(opt->letter)) return opt;
-  return NULL;
-}
+    w_assert (opt != NULL);
+    for (; opt->string != NULL; opt++)
+        if (chr == OPT_LETTER (opt->letter))
+            return opt;
 
-
-static inline const w_opt_t*
-_opt_lookup_fuzz(const w_opt_t *opt, const char *str, const char *prg)
-{
-  size_t len;
-  const w_opt_t *ret;
-  w_assert(opt != NULL);
-  w_assert(str != NULL);
-  w_assert(prg != NULL);
-
-  len = strlen(str);
-  for (; opt->string != NULL; opt++)
-    if (!strncmp(opt->string, str, len))
-      break;
-
-  if (opt->string == NULL)
     return NULL;
-
-  ret = opt++;
-
-  /* Check wether the option string is not ambiguous. */
-  for (; opt->string != NULL; opt++)
-    if (!strncmp(opt->string, str, len))
-      break;
-
-  /* If we reach the end, no other prefix is equal -> ok. */
-  if (opt->string == NULL)
-    return ret;
-
-  /* ...otherwise, we are in trouble. */
-  fprintf(stderr, "%s: option '%s' is ambiguous, possibilities:\n", prg, str);
-  for (; ret->string != NULL; ret++)
-    if (!strncmp(ret->string, str, len))
-      fprintf(stderr, "    --%s\n", ret->string);
-  fprintf(stderr, "Hint: try '%s --help'\n", prg);
-
-  exit(EXIT_FAILURE);
-  return NULL; /* Never reached -- but keeps compiler happy =) */
 }
 
 
 static inline const w_opt_t*
-_opt_lookup_long(const w_opt_t *opt, const char *str)
+_opt_lookup_fuzz (const w_opt_t *opt, const char *str, const char *prg)
 {
-  w_assert(opt != NULL);
-  w_assert(str != NULL);
-  for (; opt->string != NULL; opt++)
-    if (!strcmp(opt->string, str)) return opt;
-  return NULL;
+    size_t len;
+    const w_opt_t *ret;
+    w_assert (opt != NULL);
+    w_assert (str != NULL);
+    w_assert (prg != NULL);
+
+    len = strlen (str);
+    for (; opt->string != NULL; opt++)
+        if (!strncmp (opt->string, str, len))
+            break;
+
+    if (opt->string == NULL)
+        return NULL;
+
+    ret = opt++;
+
+    /* Check wether the option string is not ambiguous. */
+    for (; opt->string != NULL; opt++)
+        if (!strncmp (opt->string, str, len))
+            break;
+
+    /* If we reach the end, no other prefix is equal -> ok. */
+    if (opt->string == NULL)
+        return ret;
+
+    /* ...otherwise, we are in trouble. */
+    fprintf (stderr, "%s: option '%s' is ambiguous, possibilities:\n", prg, str);
+    for (; ret->string != NULL; ret++)
+        if (!strncmp (ret->string, str, len))
+            fprintf (stderr, "    --%s\n", ret->string);
+    fprintf(stderr, "Hint: try '%s --help'\n", prg);
+
+    exit (EXIT_FAILURE);
+    return NULL; /* Never reached -- but keeps compiler happy =) */
+}
+
+
+static inline const w_opt_t*
+_opt_lookup_long (const w_opt_t *opt, const char *str)
+{
+    w_assert (opt != NULL);
+    w_assert (str != NULL);
+    for (; opt->string != NULL; opt++)
+        if (!strcmp (opt->string, str)) return opt;
+    return NULL;
 }
 
 
@@ -300,93 +300,91 @@ w_opt_parse (const w_opt_t *options,
              int            argc,
              char         **argv)
 {
-  w_opt_status_t status = W_OPT_OK;
-  w_opt_context_t context = { argc, argv, NULL, userdata, NULL };
-  wbool files_only = W_NO;
-  size_t i = 1;
+    w_opt_status_t status = W_OPT_OK;
+    w_opt_context_t context = { argc, argv, NULL, userdata, NULL };
+    wbool files_only = W_NO;
+    size_t i = 1;
 
-  w_assert(options != NULL);
-  w_assert(argv != NULL);
+    w_assert (options != NULL);
+    w_assert (argv != NULL);
 
-  while (i < (unsigned) argc) {
-    if (!files_only && (argv[i][0] == '-')) {
-      /* option */
-      context.option = ((argv[i][1] == '-') && (argv[i][2] != '\0'))
-        ? _opt_lookup_long(options, &argv[i][2])
-        : ((argv[i][2] == '\0')
-            ? _opt_lookup_shrt(options,  argv[i][1])
-            : _opt_lookup_fuzz(options, &argv[i][1], _program_name(argv[0]))
-          );
+    while (i < (unsigned) argc) {
+        if (!files_only && (argv[i][0] == '-')) {
+            /* option */
+            context.option = ((argv[i][1] == '-') && (argv[i][2] != '\0'))
+                ? _opt_lookup_long (options, &argv[i][2])
+                : ((argv[i][2] == '\0')
+                   ? _opt_lookup_shrt (options,  argv[i][1])
+                   : _opt_lookup_fuzz (options, &argv[i][1], _program_name (argv[0]))
+                  );
 
-      if (context.option == NULL) {
-				status = W_OPT_BAD_ARG;
-				break;
-			}
-			if (OPT_LETTER(context.option->letter) == 'h') {
-				w_opt_help(options, stdout, _program_name(argv[0]));
-				status = W_OPT_EXIT_OK;
-				break;
-			}
-
-			if (context.option->narg >= (unsigned)(argc - i)) {
-				status = W_OPT_MISSING_ARG;
-				break;
+            if (context.option == NULL) {
+                status = W_OPT_BAD_ARG;
+                break;
+            }
+            if (OPT_LETTER (context.option->letter) == 'h') {
+                w_opt_help (options, stdout, _program_name (argv[0]));
+                status = W_OPT_EXIT_OK;
+                break;
 			}
 
-			/* Prepare context. */
-			context.argument = &argv[++i];
+            if (context.option->narg >= (unsigned) (argc - i)) {
+                status = W_OPT_MISSING_ARG;
+                break;
+            }
 
-			/* Invoke action. */
-			status = (*context.option->action)(&context);
+            /* Prepare context. */
+            context.argument = &argv[++i];
 
-			/* If --files (or similar) was given change flag and continue */
-			if (status == W_OPT_FILES) {
-				files_only = 1;
-				continue;
-			}
+            /* Invoke action. */
+            status = (*context.option->action) (&context);
 
-			/* Bail out of this is not a good status */
-			if (status != W_OPT_OK)
-				break;
+            /* If --files (or similar) was given change flag and continue */
+            if (status == W_OPT_FILES) {
+                files_only = 1;
+                continue;
+            }
 
-			/* Advance in arguments. */
-			i += context.option->narg;
-		}
-		else {
-			if (file_cb != NULL)
-				(*file_cb)((void*) argv[i++], userdata);
-			else
-				return i;
-		}
-	}
+            /* Bail out of this is not a good status */
+            if (status != W_OPT_OK)
+                break;
 
+            /* Advance in arguments. */
+            i += context.option->narg;
+        }
+        else {
+            if (file_cb != NULL)
+                (*file_cb) ((void*) argv[i++], userdata);
+            else
+                return i;
+        }
+    }
 
-	/* Perform final status-based actions. */
-	switch (status) {
-		case W_OPT_FILES: /* Nothing more to do. */
-		case W_OPT_OK:
-			break;
-		case W_OPT_BAD_ARG: /* Handle errors. */
-		case W_OPT_MISSING_ARG:
-			if (context.option == NULL)
-				fprintf(stderr, "%s: unknown option '%s'\nHint: try '%s --help'\n",
-                _program_name(argv[0]), argv[i], _program_name(argv[0]));
-			else
-				fprintf(stderr, "%s: %s --%s\nTry \"%s --help\" for more information.\n",
-						_program_name (argv[0]), (status == W_OPT_BAD_ARG)
-							? "bad argument passed to"
-							: "missing argument(s) to",
-						context.option->string, _program_name(argv[0]));
-			/* fall-through */
-		case W_OPT_EXIT_FAIL: /* Exit with the given status hint. */
-			exit(EXIT_FAILURE);
-		case W_OPT_EXIT_OK:
-			exit(EXIT_SUCCESS);
-	}
+    /* Perform final status-based actions. */
+    switch (status) {
+        case W_OPT_FILES: /* Nothing more to do. */
+        case W_OPT_OK:
+            break;
+        case W_OPT_BAD_ARG: /* Handle errors. */
+        case W_OPT_MISSING_ARG:
+            if (context.option == NULL)
+                fprintf (stderr, "%s: unknown option '%s'\nHint: try '%s --help'\n",
+                         _program_name (argv[0]), argv[i], _program_name (argv[0]));
+            else
+                fprintf (stderr, "%s: %s --%s\nTry \"%s --help\" for more information.\n",
+                         _program_name (argv[0]), (status == W_OPT_BAD_ARG)
+                            ? "bad argument passed to"
+                            : "missing argument(s) to",
+                         context.option->string, _program_name (argv[0]));
+            /* fall-through */
+        case W_OPT_EXIT_FAIL: /* Exit with the given status hint. */
+            exit (EXIT_FAILURE);
+        case W_OPT_EXIT_OK:
+            exit (EXIT_SUCCESS);
+    }
 
-	return i;
+    return i;
 }
-
 
 
 #ifndef W_OPT_PARSE_ARG_CHUNK
@@ -434,8 +432,8 @@ _w_opt_parse_file (w_parse_t *p, void *ctx)
                     args    = w_resize (args, char*, maxarg);
                 }
                 args[narg++] = (p->look == '"')
-                             ? w_parse_string (p)
-                             : w_parse_word (p);
+                    ? w_parse_string (p)
+                    : w_parse_word (p);
             }
 
             /* Not enough arguments given */
