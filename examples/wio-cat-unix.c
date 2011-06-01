d2853653018730ce7b308ba965f5e671db5f1b5c
Adrian Perez <aperez@igalia.com>
2011-06-01 19:27:25 +0200

    examples: Use the recently-introduced w_stdin/w_stdout variables

--- examples/wio-cat-unix.c
+++ examples/wio-cat-unix.c
@@ -13,21 +13,15 @@
 int
 main (int argc, char **argv)
 {
-    w_io_t *ioi = w_io_unix_open (STDIN_FILENO);
-    w_io_t *ioo = w_io_unix_open (STDOUT_FILENO);
-
     char buf[BUFFER_SIZE];
     ssize_t ret;
 
     w_unused (argc);
     w_unused (argv);
 
-    while ((ret = w_io_read (ioi, buf, BUFFER_SIZE)) > 0) {
-        w_io_write (ioo, buf, ret);
+    while ((ret = w_io_read (w_stdin, buf, BUFFER_SIZE)) > 0) {
+        w_io_write (w_stdout, buf, ret);
     }
-
-    w_obj_unref (ioi);
-    w_obj_unref (ioo);
 
     return EXIT_SUCCESS;
 }
