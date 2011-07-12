30aae9fc53f7c718205b4fab3ba0a7cbffc6549b
Adrian Perez <aperez@igalia.com>
2011-07-12 18:11:43 +0300

    wcfg: Add basic unit test

---
+++ tests/check-wcfg.c
@@ -0,0 +1,25 @@
+/*
+ * check-wcfg.c
+ * Copyright (C) 2011 Adrian Perez <aperez@igalia.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "wheel.h"
+#include <check.h>
+
+
+START_TEST (test_wcfg_load_empty)
+{
+    w_io_t *io = w_io_buf_open (NULL);
+    char *message = NULL;
+
+    w_cfg_t *cf = w_cfg_load (io, &message);
+    fail_if (cf == NULL, "w_cfg_load returned unexpected NULL");
+    fail_if (message != NULL, "w_cfg_load set an unexpected error message");
+
+    w_obj_unref (cf);
+    w_obj_unref (io);
+}
+END_TEST
+
