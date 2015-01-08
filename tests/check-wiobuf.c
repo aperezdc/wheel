89ba5732656c33d98632b7812b6b5c8ec096763c
Adrian Perez de Castro <aperez@igalia.com>
2015-01-08 22:31:52 +0200

    Avoid expansions with side-effects in tests macros

--- tests/check-wiobuf.c
+++ tests/check-wiobuf.c
@@ -105,9 +105,12 @@ END_TEST
 START_TEST (test_wio_buf_write)
 {
     w_io_t *io = w_io_buf_open (NULL);
-    ck_assert_int_eq (10, w_io_result_bytes (w_io_write (io, msg, 10)));
-    ck_assert_int_eq (10, w_io_result_bytes (w_io_write (io, msg, 10)));
-    ck_assert_int_eq (10, w_io_result_bytes (w_io_write (io, msg, 10)));
+    w_io_result_t r = w_io_write (io, msg, 10);
+    ck_assert_int_eq (10, w_io_result_bytes (r));
+    r = w_io_write (io, msg, 10);
+    ck_assert_int_eq (10, w_io_result_bytes (r));
+    r = w_io_write (io, msg, 10);
+    ck_assert_int_eq (10, w_io_result_bytes (r));
 
     w_buf_t *b = w_io_buf_get_buffer ((w_io_buf_t*) io);
     fail_if (b == NULL, "null autocreated buffer");
@@ -128,7 +131,8 @@ END_TEST
 START_TEST (test_wio_buf_format)
 {
     w_io_t *io = w_io_buf_open (NULL);
-    ck_assert_int_eq (2, w_io_result_bytes (w_io_format (io, "$i", 42)));
+    w_io_result_t r = w_io_format (io, "$i", 42);
+    ck_assert_int_eq (2, w_io_result_bytes (r));
 
     ck_assert_str_eq ("42", w_buf_str (w_io_buf_get_buffer ((w_io_buf_t*) io)));
 
